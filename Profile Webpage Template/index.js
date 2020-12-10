b64ee77f46f493d5c39dbe88440c472d82cb88b9
HouseTutoring <harsha@housetutoring.com>
2020-12-10 17:12:00 +0530

    Profile Webpage Template

--- Profile Webpage Template/index.js
+++ Profile Webpage Template/index.js
@@ -1,10 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+// Open the Navbar
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+// Close the navbar
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
