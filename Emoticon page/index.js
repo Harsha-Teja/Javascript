6c3ab8dab4c6163a7a70f457651ea3eebe5c7876
HouseTutoring <harsha@housetutoring.com>
2020-12-10 15:34:49 +0530

    Emoticon Page

--- Emoticon page/index.js
+++ Emoticon page/index.js
@@ -1,5 +1,6 @@
 const myEmojis = ["🤟", "🧠", "💪"]
 
+// Adding list of emoji's to display
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
     emojiContainer.innerHTML = ""
@@ -10,8 +11,10 @@ function renderEmojis() {
     }
 }
 
+//  Initial function Call 
 renderEmojis()
 
+// To add the emoji from back
 const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", function(){
     const emojiInput = document.getElementById("emoji-input")
@@ -22,6 +25,7 @@ pushBtn.addEventListener("click", function(){
     }
 })
 
+// To add the emoji from front
 const unshiftBtn = document.getElementById("unshift-btn")
 unshiftBtn.addEventListener("click", function(){
     const emojiInput = document.getElementById("emoji-input")
@@ -32,12 +36,14 @@ unshiftBtn.addEventListener("click", function(){
     }
 })
 
+// To remove the emoji from back
 const popBtn = document.getElementById("pop-btn")
 popBtn.addEventListener("click", function(){
     myEmojis.pop()
     renderEmojis()
 })
 
+// To remove the emoji from front
 const shiftBtn = document.getElementById("shift-btn")
 shiftBtn.addEventListener("click", function(){
     myEmojis.shift()
